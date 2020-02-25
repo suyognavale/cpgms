140bff6a2c335df6e7a8e3ccfea4532c5ac31db9
Suyog Navale <60667163+suyognavale@users.noreply.github.com>
2020-02-25 10:35:16 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{ 
+float r,a,c;
+printf ("ENTER THE RADIUS OF THE CIRCLE \n");
+scanf ("%f",&r);
+a=3.141*r*r;
+c=2*3.141*r;
+printf ("THE AREA OF THE CIRCLE IS: %f \n",a);
+printf ("THE CIRCUMFERANCE OF THE CIRCLE: %f",c);
+}

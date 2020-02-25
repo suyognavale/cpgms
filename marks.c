06ca13e66cb51e70c8e495ab2524b9f49e62fdb5
Suyog Navale <60667163+suyognavale@users.noreply.github.com>
2020-02-25 10:21:41 +0530

    Update marks.c

--- marks.c
+++ marks.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 void main()
 { 
-int m1,m2,m3,sum;
+int m1,m2,m3,m4,m5,sum;
 float avg;
 char name[20];
-printf("ENTER YOUR NAME: \n");
+printf("ENTER YOUR NAME \n");
 scanf("%s", name);
-printf ("enter your m1 m2 m3 marks \n");
-scanf ("%d %d %d",&m1 ,&m2, &m3);
-sum= m1+m2+m3;
-avg=(sum/3.0);
-printf ("your name is %s \n",name);
-printf("ur total marks scored is %d",sum);
-printf ("your average : ");
+printf ("enter your m1 \n");
+scanf ("%d",&m1);
+printf ("enter your m2 \n");
+scanf ("%d",&m2);
+printf ("enter your m3 \n");
+scanf ("%d",&m3);
+printf ("enter your m4 \n");
+scanf ("%d",&m4);
+printf ("enter your m5 \n");
+scanf ("%d",&m5);
+sum= m1+m2+m3+m4+m5;
+avg=(sum/5.0);
+printf ("Your name is: %s \n",name);
+printf("Your total marks scored is %d \n",sum);
+printf ("Your average : ");
 printf ("%f",avg);
 
 }

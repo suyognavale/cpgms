28ff7c39bfdbe3ae96448622d914b8e7503a7fe8
Suyog Navale <60667163+suyognavale@users.noreply.github.com>
2020-02-25 09:25:45 +0530

    Add files via upload

---
+++ relationoperations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{ 
+int a,b;
+printf ("ENTER THE VALUE OF A AND B");
+scanf ("%d %d",&a,&b);
+if(a>b)
+{
+printf ("b is smaller than a");
+}
+else
+{
+printf ("b is bigger than a");
+}
+}

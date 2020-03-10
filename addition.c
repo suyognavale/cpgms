9332b201be00ac2ca2300804e729e47e537457c7
Suyog Navale <60667163+suyognavale@users.noreply.github.com>
2020-03-10 10:05:20 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+float a,b,c;
+
+printf ("ENTER THE VALUE OF a\n");
+scanf ("%f" ,&a);
+printf ("ENTER THE VALUE OF b\n");
+scanf ("%f",&b);
+c=a+b;
+printf ("THE ANSWER IS:%f\n",c);
+
+}
+
+
